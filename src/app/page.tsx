27b55f0fa3f0b7a32a6534e8e4607d6696cf7acc
zehan khan <zehan9211@gmail.com>
2024-05-20 18:31:24 +0530

    feat: add next-themes and implement theme toggle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
+import { ThemeToggle } from "@/components/common/theme-toggle";
 import Image from "next/image";
 
 export default function Home() {
-    return <main>app module</main>;
+    return <main>app module
+        <div>
+            <ThemeToggle />
+        </div>
+    </main>;
 }
